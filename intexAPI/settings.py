150763939c18bdeac064897708b10ed2ee64cc86
Sarah Fankhauser <scfank99@byu.edu>
2020-04-09 14:43:12 -0600

    static files

--- intexAPI/settings.py
+++ intexAPI/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = { 
